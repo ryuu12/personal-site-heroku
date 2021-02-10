53e9838d5e75946452da574ab912c7b68dbcbdbc
Ryuu12 <antianobl@gmail.com>
2021-02-10 15:50:27 +0700

    add table

--- RayhanNoGabut/settings.py
+++ RayhanNoGabut/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +122,5 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
